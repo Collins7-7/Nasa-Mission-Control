0b82e22a8fd24772033375b75c97176a9859844e
COllins <collinsmuiruri77@gmail.com>
2023-07-26 15:41:08 +0300

    Enables planets GET functionality

---
+++ Server/src/routes/planets/planets.router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { getPlanets } = require("./planets.controllers");
+
+const planetsRouter = express.Router();
+
+planetsRouter.get("/planets", getPlanets);
+
+module.exports = planetsRouter;
